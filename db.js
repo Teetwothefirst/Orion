40fb3bde600fe62b06b6699250d400b5ff0af7d4
Teetwothefirst <dexterdavid835@gmail.com>
2025-05-22 13:37:33 -0700

    firebase auth with phone numbers

--- db.js
+++ db.js
@@ -19,28 +19,51 @@
 // });
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dexterdavid835:oikHhtXUckk54Gsk@orion.d76iprj.mongodb.net/?retryWrites=true&w=majority&appName=Orion";
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://dexterdavid835:oikHhtXUckk54Gsk@orion.d76iprj.mongodb.net/?retryWrites=true&w=majority&appName=Orion";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjVR_VCEBsTwtXVAHs_sZrHScZlXw2WIY",
+  authDomain: "drive-4cdc9.firebaseapp.com",
+  projectId: "drive-4cdc9",
+  storageBucket: "drive-4cdc9.firebasestorage.app",
+  messagingSenderId: "424984560826",
+  appId: "1:424984560826:web:c0f6dea27960b244446e83",
+  measurementId: "G-5J4BMCYHBV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
